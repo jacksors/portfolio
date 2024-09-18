1e5fd6bf5daad37a12ed8909e87f75975d0c1cb2
Jackson Stone <jackson@jstone.us>
2024-09-17 20:17:07 -0500

    Update button styling

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,14 +10,14 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-gradient-to-tl text-primary-foreground shadow",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-[#3A6073] hover:text-primary-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
